bce3a6a2c12af6f89db168217fe4807018aa42a8
Ankit Pandey <153378791+Mr-Ankitpandey@users.noreply.github.com>
2025-07-22 13:43:16 +0000

    [LeetCode Sync] Runtime - 2 ms (12.98%), Memory - 42.6 MB (99.98%)

---
+++ 0065-valid-number/solution.java
@@ -0,0 +1,35 @@
+public class Solution {
+    public boolean isNumber(String s) {
+        s = s.trim();
+        boolean num = false, dot = false, exp = false;
+        int n = s.length();
+
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+
+            if (Character.isDigit(c)) {
+                num = true;
+            } else if (c == '+' || c == '-') {
+                if (i != 0 && s.charAt(i - 1) != 'e' && s.charAt(i - 1) != 'E') {
+                    return false;
+                }
+            } else if (c == '.') {
+                if (dot || exp) {
+                    return false;
+                }
+                dot = true;
+            } else if (c == 'e' || c == 'E') {
+                if (exp || !num) {
+                    return false;
+                }
+                exp = true;
+                num = false;
+            } else {
+                return false;
+            }
+        }
+
+        return num;
+    }
+}
+
