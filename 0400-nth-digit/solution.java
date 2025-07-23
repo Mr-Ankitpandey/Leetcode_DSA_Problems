0863df9e154eb744a2a0c23601bbe5130d80ffef
Ankit Pandey <153378791+Mr-Ankitpandey@users.noreply.github.com>
2025-07-23 16:17:05 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 40.4 MB (100.00%)

---
+++ 0400-nth-digit/solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int findNthDigit(int n) {
+        int digitLength = 1;
+        long count = 9;
+        int start = 1;
+
+        while (n > digitLength * count) {
+            n -= digitLength * count;
+            digitLength++;
+            count *= 10;
+            start *= 10;
+        }
+
+        int number = start + (n - 1) / digitLength;
+        String numberStr = Integer.toString(number);
+        return Character.getNumericValue(numberStr.charAt((n - 1) % digitLength));
+    }
+}
+
