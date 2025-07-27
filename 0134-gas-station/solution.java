2a9482c8c482813ed596aaa79f823d7133d4f09d
Ankit Pandey <153378791+Mr-Ankitpandey@users.noreply.github.com>
2025-07-27 13:14:23 +0000

    [LeetCode Sync] Runtime - 2 ms (99.24%), Memory - 56.4 MB (100.00%)

---
+++ 0134-gas-station/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0, currTank = 0, start = 0;
+
+        for (int i = 0; i < gas.length; i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            currTank += diff;
+
+            if (currTank < 0) {
+                start = i + 1;
+                currTank = 0;
+            }
+        }
+
+        return total >= 0 ? start : -1;
+    }
+}
+
