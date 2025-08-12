883ac9e1682bd71425edc0561d186a2a23691382
Ankit Pandey <153378791+Mr-Ankitpandey@users.noreply.github.com>
2025-08-12 18:02:53 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 41.3 MB (99.97%)

---
+++ 0198-house-robber/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int rob(int[] nums) {
+        int rob = 0, skip = 0;
+        for (int num : nums) {
+            int newRob = skip + num;
+            int newSkip = Math.max(skip, rob);
+            rob = newRob;
+            skip = newSkip;
+        }
+        return Math.max(rob, skip);
+    }
+}
+
