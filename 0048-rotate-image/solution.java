515b93b3b4e9bad1020bf75bf720066fd9ee02de
Ankit Pandey <153378791+Mr-Ankitpandey@users.noreply.github.com>
2025-08-28 09:15:52 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.3 MB (99.97%)

---
+++ 0048-rotate-image/solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+
+        //Transpose
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
+            }
+        }
+
+        //Reverse each row
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n - 1 - j];
+                mat[i][n - 1 - j] = temp;
+            }
+        }
+    }
+}
+
